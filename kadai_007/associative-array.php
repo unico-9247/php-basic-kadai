9b9eaf0d588dbb4fa7a6a62c2ad48404e42c8fb7
unico-9247 <131856258+unico-9247@users.noreply.github.com>
2023-07-02 05:36:15 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>課題7
+  </title>
+</head>
+
+<body>
+  <?php
+  // 連想配列に値を代入する
+  $data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+  // 連想配列の値を出力する
+  print_r($data);
+  ?>
+</body>
+</html>
