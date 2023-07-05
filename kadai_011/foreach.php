b49881d92b9a6f0b1ddc9cb3638fc1182868078e
unico-9247 <131856258+unico-9247@users.noreply.github.com>
2023-07-06 06:33:33 +0900

    kadai_011をアップロード

---
+++ kadai_011/foreach.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>課題11</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $food_data = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+    foreach($food_data as $key => $value) {
+      echo "{$key}: {$value}<br>";
+    }
+    ?>
+  </p>
+  
+</body>
+</html>
